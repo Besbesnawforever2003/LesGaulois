2e9ffbe46c7afd2f139cf709e79dc085b9e84682
Besbesnawforever2003 <wassimwali700@gmail.com>
2025-01-23 10:49:50 +0100

    EX1_1

--- src/personnage/Gaulois.java
+++ src/personnage/Gaulois.java
@@ -1,5 +1,27 @@
 package personnage;
 
+
 public class Gaulois {
+
+ private String nom;
+
+ private int force;
+
+
+ public Gaulois(String nom, int force) {
+
+ this.nom = nom;
+
+ this.force = force;
+
+ }
+
+
+ public String getNom() {
+
+	 
+ return nom;
+
+ }
 
 }
